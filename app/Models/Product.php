8d42f17f37fbff5ce402bb5a6b426311d5a86852
Gamaliel <agamalial@gmail.com>
2026-08-14 11:42:49 +0700

    add correct fillable

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,11 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-#[FIllable('category_id', 'name', 'description', 'price', 'image', 'stock', 'is_active')]
+// #[FIllable('category_id', 'name', 'description', 'price', 'image', 'stock', 'is_active')]
 
 class Product extends Model
 {
     //
+    protected $fillable = [
+        'category_id',
+        'name',
+        'description',
+        'price',
+        'image',
+        'stock',
+        'is_active',
+    ];
     protected function castable(): array
     {
         return [
